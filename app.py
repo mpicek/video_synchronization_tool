20aafeaa05d2add6845d4bbcb00709994694a21b
mpicek <martinpicek4@gmail.com>
2023-09-26 09:31:06 +0200

    Add text box for specifying the frame

--- app.py
+++ app.py
@@ -7,21 +7,35 @@ import os
 import ffmpeg
 from tqdm import tqdm
 
+INITIAL_DIR = '/home/mpicek/repos/Trunk_KinRec/data/HON006/250Hz_L5/'
+
 class App:
     def __init__(self, master, same_fps):
         self.master = master
         self.frame1 = 0
         self.frame2 = 0
-        self.video_path1 = filedialog.askopenfilename(initialdir='/home/mpicek/Downloads/Recordings_cp/')
-        self.video_path2 = filedialog.askopenfilename(initialdir='/home/mpicek/Downloads/Recordings_cp/')
+        self.video_path1 = filedialog.askopenfilename(initialdir=INITIAL_DIR)
+        self.video_path2 = filedialog.askopenfilename(initialdir=INITIAL_DIR)
+
+        print(self.video_path1)
+        print(self.video_path2)
 
         self.cap1, fps1 = self.load_video(self.video_path1)
         self.cap2, fps2 = self.load_video(self.video_path2)
 
+        original_fps1 = fps1
+        original_fps2 = fps2
+
+        fps1 = round(fps1)
+        fps2 = round(fps2)
+
         target_fps = None
         self.temp_file_path = None
 
-        if same_fps:
+        # predefined_fps = 30
+        predefined_fps = None
+
+        if predefined_fps is None and same_fps:
             # we need both videos to have the same FPS
             target_fps = min(fps1, fps2)
             if fps1 > fps2:
@@ -34,17 +48,31 @@ class App:
                 self.temp_file_path = self.convert_fps(self.video_path2, target_fps=target_fps)
                 self.cap2.release()
                 self.cap2, fps2 = self.load_video(self.temp_file_path)
+            else:
+                print(f"The first video has {fps1} FPS, and the second has {fps2} FPS. No conversion needed.")
+        
+        if predefined_fps is not None:
+            self.temp_file_path = self.convert_fps(self.video_path1, target_fps=predefined_fps)
+            self.cap1.release()
+            self.cap1, fps1 = self.load_video(self.temp_file_path)
 
-        self.target_fps1 = fps1
-        self.target_fps2 = fps2
+            self.temp_file_path = self.convert_fps(self.video_path2, target_fps=predefined_fps)
+            self.cap2.release()
+            self.cap2, fps2 = self.load_video(self.temp_file_path)
+
+        self.target_fps1 = original_fps1 if abs(fps1 - original_fps1) < 1 else fps1
+        self.target_fps2 = original_fps2 if abs(fps2 - original_fps2) < 1 else fps2
+        print(self.target_fps1)
+        print(self.target_fps2)
+        # self.target_fps1 = self.load_video(self.video_path1)
+        # self.target_fps2 = self.load_video(self.video_path2)
         
         self.total_frames_video1 = int(self.cap1.get(cv2.CAP_PROP_FRAME_COUNT))
         self.total_frames_video2 = int(self.cap2.get(cv2.CAP_PROP_FRAME_COUNT))
 
-
         # GUI setup
-        self.canvas1 = tk.Canvas(master, width=500, height=500)
-        self.canvas2 = tk.Canvas(master, width=500, height=500)
+        self.canvas1 = tk.Canvas(master, width=500, height=700)
+        self.canvas2 = tk.Canvas(master, width=500, height=700)
         self.canvas1.grid(row=0, column=0)
         self.canvas2.grid(row=0, column=1)
 
@@ -79,11 +107,26 @@ class App:
         self.button2_left_one.grid(row=1, column=1, sticky='w')
         self.button2_right_one.grid(row=1, column=1, sticky='e')
 
+
+        self.frame_entry1 = tk.Entry(master)
+        self.frame_entry1.grid(row=4, column=0)
+        self.frame_entry1.bind('<Return>', lambda event: self.jump_to_frame(event, 1))
+
+        self.frame_entry2 = tk.Entry(master)
+        self.frame_entry2.grid(row=4, column=1)
+        self.frame_entry2.bind('<Return>', lambda event: self.jump_to_frame(event, 2))
+
         self.frame_label1 = tk.Label(master)
-        self.frame_label1.grid(row=4, column=0)
+        self.frame_label1.grid(row=5, column=0)
 
         self.frame_label2 = tk.Label(master)
-        self.frame_label2.grid(row=4, column=1)
+        self.frame_label2.grid(row=5, column=1)
+
+        self.time_label1 = tk.Label(master, text="")
+        self.time_label1.grid(row=6, column=0)
+
+        self.time_label2 = tk.Label(master, text="")
+        self.time_label2.grid(row=6, column=1)
 
         self.ok_button = tk.Button(master, text="OK", command=self.run_ffmpeg)
         self.ok_button.grid(row=4, column=0, columnspan=2)
@@ -99,10 +142,26 @@ class App:
         return cap, fps
 
     def convert_fps(self, video_path, target_fps):
-        temp_file_path = os.path.join(os.path.dirname(video_path), "temp_same_fps_video_file.mp4")
+        temp_file_path = os.path.join(os.path.dirname(video_path), video_path.split('/')[-1] + "_temp_same_fps_video_file.mp4")
+        print(temp_file_path)
         ffmpeg.input(video_path).output(temp_file_path, r=target_fps).run()
 
         return temp_file_path
+    
+    def resize_image(self, image, max_width, max_height):
+        original_width, original_height = image.size
+        aspect_ratio = original_width / original_height
+        
+        if aspect_ratio > 1:
+            # Image is wider than tall
+            new_width = min(original_width, max_width)
+            new_height = int(new_width / aspect_ratio)
+        else:
+            # Image is taller than wide or square
+            new_height = min(original_height, max_height)
+            new_width = int(new_height * aspect_ratio)
+    
+        return image.resize((new_width, new_height), Image.ANTIALIAS)
 
     def show_frame(self):
         self.cap1.set(cv2.CAP_PROP_POS_FRAMES, self.frame1)
@@ -114,12 +173,18 @@ class App:
         self.frame_label1.config(text=f"Current frame: {self.frame1}")
         self.frame_label2.config(text=f"Current frame: {self.frame2}")
 
+        self.time_label1.config(text=f"Current time: {round(self.frame1 / self.target_fps1, 3)} s")
+        self.time_label2.config(text=f"Current time: {round(self.frame2 / self.target_fps2, 3)} s")
+
         frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
         frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
 
         # Resize to fit in 500x500 canvas
-        frame1 = Image.fromarray(frame1).resize((500, 500))
-        frame2 = Image.fromarray(frame2).resize((500, 500))
+        frame1 = Image.fromarray(frame1)
+        frame2 = Image.fromarray(frame2)
+
+        frame1 = self.resize_image(frame1, 500, 700)
+        frame2 = self.resize_image(frame2, 500, 700)
 
         self.image1 = ImageTk.PhotoImage(frame1)
         self.image2 = ImageTk.PhotoImage(frame2)
@@ -127,12 +192,30 @@ class App:
         self.canvas1.create_image(0, 0, image=self.image1, anchor='nw')
         self.canvas2.create_image(0, 0, image=self.image2, anchor='nw')
     
+    def jump_to_frame(self, event, video_index):
+        if video_index == 1:
+            entry = self.frame_entry1
+            self.frame1 = int(entry.get())
+            if self.frame1 < 0:
+                self.frame1 = 0
+            elif self.frame1 >= self.total_frames_video1:
+                self.frame1 = self.total_frames_video1 - 2
+            self.show_frame()
+        elif video_index == 2:
+            entry = self.frame_entry2
+            self.frame2 = int(entry.get())
+            if self.frame2 < 0:
+                self.frame2 = 0
+            elif self.frame2 >= self.total_frames_video2:
+                self.frame2 = self.total_frames_video2 - 2
+            self.show_frame()
+
     def skip_frames1(self, frames):
         self.frame1 += frames
         if self.frame1 < 0:
             self.frame1 = 0
         elif self.frame1 >= self.total_frames_video1:
-            self.frame1 = self.total_frames_video1 - 1
+            self.frame1 = self.total_frames_video1 - 2
         self.show_frame()
 
     def skip_frames2(self, frames):
@@ -140,7 +223,7 @@ class App:
         if self.frame2 < 0:
             self.frame2 = 0
         elif self.frame2 >= self.total_frames_video2:
-            self.frame2 = self.total_frames_video2 - 1
+            self.frame2 = self.total_frames_video2 - 2
         self.show_frame()
 
 
